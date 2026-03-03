3117f62265f191efb1da10f89453dc406581108b
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-03-03 16:13:15 +0530

    Add video to playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   //TODO: create playlist
@@ -87,6 +88,47 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+  // get playlistId, videoId from req.params
+  // check playlistId and videoId are valid objectid
+  // check if video is there in the db
+  // find the playlist with the playlistId with the owner
+  // set the videos to videoId
+  // check the playlist
+  // return response
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid Playlist Id");
+  }
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid Video Id");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  const playlist = await Playlist.findOneAndUpdate(
+    { _id: playlistId, owner: req.user._id },
+    {
+      $addToSet: {
+        videos: videoId,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found or you are not the owner");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, "Video added to playlist successfully")
+    );
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
