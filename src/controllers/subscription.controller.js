df96adfcf53c5bb36095e137bdc8cc302187a89d
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-03-05 12:01:45 +0530

    Add toggle subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,28 +1,84 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+  // TODO: toggle subscription
+  // get channelId from req.params
+  // check channelid is a valid object id
+  // check channel user exists in the user model
+  // get subscriberId from req.user._id
+  // check if channelid is eqaul to userid then it is not possible because a user A can subscribe or unsubscribe to user B not itself.
+  // if they are equal reject the request
+  // check if subscription already exists
+  // search the subscription model and filter the documents based on the channelId and subscriberId
+  // if it exists in the subscription document then we need to delete it
+  // else if it does not exist we need to create a new document
+  // if it was delted then we return response that we unsubscribed the channel
+  // if it was created then we return response that we subscribed the channel
+
+  const { channelId } = req.params;
+  const subscriberId = req.user?._id;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid Channel Id ");
+  }
+
+  const isChannelExist = await User.findById(channelId);
+  if (!isChannelExist) {
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  if (subscriberId.toString() === channelId) {
+      throw new ApiError(400, "You cannot subscribe to your own channel");
+    }
+
+  const isSubscriptionExist = await Subscription.findOne({
+    subscriber: subscriberId,
+    channel: channelId,
+  });
+
+  if (isSubscriptionExist) {
+    await Subscription.findOneAndDelete({
+      subscriber: subscriberId,
+      channel: channelId,
+    });
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { isSubscribed: false },
+          "Unsubscribed Successfully"
+        )
+      );
+  } else {
+    const createSubscription = await Subscription.create({
+      subscriber: subscriberId,
+      channel: channelId,
+    });
+    if (!createSubscription) {
+      throw new ApiError(400, "Error while creating the subscription");
+    }
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(201, { isSubscribed: true }, "Subscribed Successfully")
+      );
+  }
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+  const { channelId } = req.params;
+});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+  const { subscriberId } = req.params;
+});
 
-export {
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels
-}
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
